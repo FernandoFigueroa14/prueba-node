6dc20426a66f431c49a0060278645b756f4bf5a3
FernandoFigueroa14 <luisfersurfer14@hotmail.com>
2021-05-27 01:29:48 -0500

    Añadi funcion para mi novia bonita

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hola mundo!");
+//console.log("Hola mundo!");
+
+let noviabb = ["MI VIDA", "MI NIÑA", "MI ESPOSA BONITA", "MI NOVIA HERMOSA"];
+for(let j=0; j<=200;j++){
+    for(let i=0; i<noviabb.length; i++){
+        console.log("TE AMO MUCHO " + noviabb[i] + " <3");
+    }
+}
